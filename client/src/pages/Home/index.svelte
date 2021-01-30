615de52b06bc72488876910caad14ce36ac3f2f3
Harsh Agrawal <itsharshag@gmail.com>
2021-01-31 00:38:36 +0530

    Update Home page design

--- client/src/pages/Home/index.svelte
+++ client/src/pages/Home/index.svelte
@@ -7,6 +7,55 @@
 
     export let location
 
+    const videoGames = [
+        {
+            name: "Globulas",
+            imgSrc: "https://play-lh.googleusercontent.com/8UBQKDgQZoygD5UQpiZNd4EQlkom1W7ECM4qUKoZyWN3iInSsByeDH3LnpKJ3GnpFA=w720-h310-rw",
+            link: "https://play.google.com/store/apps/details?id=com.GenysisStudios.Globulas"
+        },
+        {
+            name: "The Knight's Call",
+            imgSrc: "https://play-lh.googleusercontent.com/HBms7AIqtffyplfEHDEoOPYp_0QKMd94isHZxDuX_-ixZsL2omJDYV8ZK52ngT1UT4Y=w720-h310-rw",
+            link: "https://play.google.com/store/apps/details?id=com.GenysisStudios.TheKnightsCall"
+        },
+        {
+            name: "Knight Run",
+            imgSrc: "https://play-lh.googleusercontent.com/V70dE-gHhrBFjEMIgUMe8R7ZwFXI6ebwN6DewxEFVTG9B5zjYzmDItfoZOBllrDpgDA=w720-h310-rw",
+            link: "https://play.google.com/store/apps/details?id=com.GenysisStudios.KnightRun&hl=en_CA"
+        }
+    ]
+
+    const webApplications = [
+        {
+            name: "The Book Hub",
+            imgSrc: "https://live.staticflickr.com/65535/50891175782_11114ecf02_b.jpg",
+            link: "https://www.thebookhub.org"
+        },
+        {
+            name: "Harsh Agrawal",
+            imgSrc: "https://live.staticflickr.com/65535/50891329557_98f1d996b9_b.jpg",
+            link: "https://itsharshag.com"
+        }
+    ]
+
+    const generativeArt = [
+        {
+            name: "Concentric Circles",
+            imgSrc: "https://live.staticflickr.com/65535/50891563047_6127fb9bbf_w.jpg",
+            link: "https://editor.p5js.org/HarshAgwl/sketches/13f1De2xr"
+        },
+        {
+            name: "Recursion",
+            imgSrc: "https://live.staticflickr.com/65535/50891446376_c3290d6f83_w.jpg",
+            link: "https://editor.p5js.org/HarshAgwl/sketches/13f1De2xr"
+        },
+        {
+            name: "Om",
+            imgSrc: "https://live.staticflickr.com/65535/50891562922_5af82f96df_w.jpg",
+            link: "https://editor.p5js.org/HarshAgwl/sketches/hWpwgg9i2"
+        }
+    ]
+
     let posts = []
     let hasPageLoaded = false
 
@@ -55,7 +104,6 @@
         navigate(`/?name=${paginationDetails.name}&currentId=${paginationDetails.currentId}&dir=${dir}&pageNo=${pageNo}`)
     }
 
-
     $: {
         loadItems(location)
     }
@@ -65,50 +113,146 @@
     .post .title{
         cursor: pointer;
     }
+    div.items{
+        margin-bottom: 45px;
+    }
+    .items .grid{
+        display: grid;
+        grid-template-columns: repeat(2, 50%);
+        grid-gap: 10px;
+    }
+    .item{
+        height: 220px;
+        background-position: center;
+        background-size: cover;
+    }
+    .overlay{
+        height: 100%;
+        width: 100%;
+        padding: 20px;
+        font-size: 60px;
+        opacity: 0;
+        background: rgba(0,0,0,0.65);
+        color: white;
+        transition: 0.4s;
+        vertical-align: bottom;
+        margin-top: auto;
+        display: flex;
+        flex-direction: column-reverse;
+    }
+    .item:hover .overlay{
+        opacity: 1;
+        cursor: pointer;
+    }
+    .video-games .item:nth-child(1){
+        grid-column: 1/3;
+    }
+    h1.title{
+        font-family: 'Red Hat Display', cursive;
+        text-transform: lowercase;
+        padding-bottom: 20px;
+    }
+    .generative-art .grid, .latest-blog-posts .grid{
+        grid-template-columns: repeat(3, 33%);
+
+    }
+    .posts h1.title{
+        padding-bottom: 0px;
+    }
+    .latest-blog-posts .grid{
+        grid-gap: 20px;
+    }
+    .latest-blog-posts .post{
+        height: fit-content;
+        margin: 0px;
+        padding: 0px;
+    }
+    @media (max-width: 500px){
+        .video-games .item:nth-child(1){
+            grid-column: 1/2;
+        }
+        .grid{
+            grid-template-columns: repeat(1, 99%)!important;
+        }
+    }
+    @media (max-width: 960px){
+        .latest-blog-posts .grid{
+            grid-template-columns: repeat(2, 49%);
+        }
+    }
 </style>
 
 <Content class="content">
-    <div class="posts">
-        {#if hasPageLoaded}
-            {#each posts as post}
-                <div class="post">
-                    <h2 
-                        class="title"
-                        on:click={() => navigate(`/post/${post._id}`)}
-                    >
-                        {post.title}
-                    </h2>
-                    <p class="date">
-                        {makeCreatedOnUserFriendly(post.createdOn)}
-                    </p>
-                    <div class="teaser">
-                        {@html post.teaser}
+    <div class="video-games items">
+        <h1 class="title">Fun Interactive experience aka Video Games</h1>
+        <div class="grid">
+            {#each videoGames as item}
+                <div 
+                    class="item"
+                    style={`background-image: url("${item.imgSrc}");`}
+                    on:click={() => window.open(item.link, "_blank")}
+                >
+                    <div class="overlay">
+                        {item.name}
                     </div>
                 </div>
             {/each}
-                <Button 
-                    on:click={() => switchPage("back")}
-                    disabled={paginationDetails.pageNo <= 1}
+        </div>
+    </div>
+    <div class="generative-art items">
+        <h1 class="title">Art pieces made with Code aka Generative Art</h1>
+        <div class="grid">
+            {#each generativeArt as item}
+                <div 
+                    class="item"
+                    style={`background-image: url("${item.imgSrc}");`}
+                    on:click={() => window.open(item.link, "_blank")}
                 >
-                    Back
-                </Button>
-                <Button 
-                    on:click={() => switchPage("next")}
-                    disabled={posts.length < paginationDetails.searchResultSize}>
-                    Next
-                </Button>
-            {:else}
-                <SkeletonPlaceholder style="height: 12rem; width: 80%;" />
-                <br/>
-                <SkeletonPlaceholder style="height: 12rem; width: 80%;" />
-                <br/>
-                <SkeletonPlaceholder style="height: 12rem; width: 80%;" />
-                <br/>
-                <SkeletonPlaceholder style="height: 12rem; width: 80%;" />
-                <br/>
-                <SkeletonPlaceholder style="height: 12rem; width: 80%;" />
-                <br/>
-                <SkeletonPlaceholder style="height: 12rem; width: 80%;" />
-        {/if}
+                    <div class="overlay">
+                        {item.name}
+                    </div>
+                </div>
+            {/each}
+        </div>
+    </div>
+    <div class="web-applications items">
+        <h1 class="title">Interactive websites aka Web Applications</h1>
+        <div class="grid">
+            {#each webApplications as item}
+                <div 
+                    class="item"
+                    style={`background-image: url("${item.imgSrc}");`}
+                    on:click={() => window.open(item.link, "_blank")}
+                >
+                    <div class="overlay">
+                        {item.name}
+                    </div>
+                </div>
+            {/each}
+        </div>
+    </div>
+    <br/>
+    <div class="latest-blog-posts items">
+        <h1 class="title">latest blog posts</h1>
+        <div class="grid">
+            {#each posts.splice(0,3) as item}
+                <div 
+                    class="item post"
+                >
+                <h2 
+                class="title"
+                on:click={() => navigate(`/post/${item._id}`)}
+            >
+                {item.title}
+            </h2>
+            <p class="date">
+                {makeCreatedOnUserFriendly(item.createdOn)}
+            </p>
+            <div class="teaser">
+                {@html item.teaser}
+            </div>
+                </div>
+            {/each}
+        </div>
     </div>
 </Content>
